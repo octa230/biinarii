00c55d3aa13a1316a5ad38f4b037195f4ec302fc
octa230 <mervynstunner@outlook.com>
2022-11-11 00:11:16 +0300

    other product bug changes

--- frontend/src/components/Products/Product.jsx
+++ frontend/src/components/Products/Product.jsx
@@ -6,8 +6,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToWishlist, removeFromWishlist } from '../../actions/wishlistAction';
 import { useSnackbar } from 'notistack';
 
-const Product = ({ _id, name, images, ratings, numOfReviews, price, cuttedPrice }) => {
-
+const Product = (props) => {
+    const product = props
     
     const dispatch = useDispatch();
     const { enqueueSnackbar } = useSnackbar();
@@ -29,11 +29,11 @@ const Product = ({ _id, name, images, ratings, numOfReviews, price, cuttedPrice 
     return (
         <div className="flex flex-col items-start gap-2 px-4 py-6 relative hover:shadow-lg rounded-sm">
             {/* <!-- image & product title --> */}
-            <Link to={`/product/${_id}`} className="flex flex-col items-center text-center group">
+            <Link to={`/product/${product._id}`} className="flex flex-col items-center text-center group">
                 <div className="w-44 h-48">
-                    <img draggable="false" className="w-full h-full object-contain" src={images && images[0].url} alt="" />
+                    <img draggable="false" className="w-full h-full object-contain" src={product.images} alt="" />
                 </div>
-                <h2 className="text-sm mt-4 group-hover:text-primary-blue text-left">{name.length > 85 ? `${name.substring(0, 85)}...` : name}</h2>
+                <h2 className="text-sm mt-4 group-hover:text-primary-blue text-left">{product.name.length > 85 ? `${product.name.substring(0, 85)}...` : product.name}</h2>
             </Link>
             {/* <!-- image & product title --> */}
 
@@ -41,23 +41,25 @@ const Product = ({ _id, name, images, ratings, numOfReviews, price, cuttedPrice 
             <div className="flex flex-col gap-2 items-start">
                 {/* <!-- rating badge --> */}
                 <span className="text-sm text-gray-500 font-medium flex gap-2 items-center">
-                    <span className="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">{ratings.toFixed(1)} <StarIcon sx={{ fontSize: "14px" }} /></span>
-                    <span>({numOfReviews})</span>
+                    <span className="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">{product.ratings.toFixed(1)} <StarIcon sx={{ fontSize: "14px" }} /></span>
+                    <span>({product.numOfReviews})</span>
                 </span>
                 {/* <!-- rating badge --> */}
 
                 {/* <!-- price container --> */}
                 <div className="flex items-center gap-1.5 text-md font-medium">
-                    <span>UGX: {price.toLocaleString()}</span>
-                    <span className="text-gray-500 line-through text-xs">UGX: {cuttedPrice.toLocaleString()}</span>
-                    <span className="text-xs text-primary-green">{getDiscount(price, cuttedPrice)}%&nbsp;off</span>
+                    <span>UGX: {product.price.toLocaleString()}</span>
+                    <span className="text-gray-500 line-through text-xs">UGX: {product.cuttedPrice.toLocaleString()}</span>
+                    <span className="text-xs text-primary-green">{getDiscount(product.price, product.cuttedPrice)}%&nbsp;off</span>
                 </div>
                 {/* <!-- price container --> */}
             </div>
             {/* <!-- product description --> */}
 
             {/* <!-- wishlist badge --> */}
-            <span onClick={addToWishlistHandler} className={`${itemInWishlist ? "text-red-500" : "hover:text-red-500 text-gray-300"} absolute top-6 right-6 cursor-pointer`}><FavoriteIcon sx={{ fontSize: "18px" }} /></span>
+            <span onClick={addToWishlistHandler} 
+            className={`${itemInWishlist ? "text-red-500" : "hover:text-red-500 text-gray-300"} 
+            absolute top-6 right-6 cursor-pointer`}><FavoriteIcon sx={{ fontSize: "18px" }} /></span>
             {/* <!-- wishlist badge --> */}
 
         </div>
