3b9d12c224dbe768e708390444e096b62bfdcae5
octa230 <mervynstunner@outlook.com>
2022-11-11 09:10:10 +0300

    changes

--- frontend/src/components/Products/Product.jsx
+++ frontend/src/components/Products/Product.jsx
@@ -6,22 +6,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToWishlist, removeFromWishlist } from '../../actions/wishlistAction';
 import { useSnackbar } from 'notistack';
 
-const Product = (props) => {
-    const product = props
+const Product = ({_id, name, images, price, cuttedPrice }) => {
     
     const dispatch = useDispatch();
     const { enqueueSnackbar } = useSnackbar();
 
     const { wishlistItems } = useSelector((state) => state.wishlist);
 
-    const itemInWishlist = wishlistItems.some((i) => i.product === product._id);
+    const itemInWishlist = wishlistItems.some((i) => i.product === _id);
 
     const addToWishlistHandler = () => {
         if (itemInWishlist) {
-            dispatch(removeFromWishlist(product._id));
+            dispatch(removeFromWishlist(_id));
             enqueueSnackbar("Remove From Wishlist", { variant: "success" });
         } else {
-            dispatch(addToWishlist(product._id));
+            dispatch(addToWishlist(_id));
             enqueueSnackbar("Added To Wishlist", { variant: "success" });
         }
     }
@@ -29,11 +28,11 @@ const Product = (props) => {
     return (
         <div className="flex flex-col items-start gap-2 px-4 py-6 relative hover:shadow-lg rounded-sm">
             {/* <!-- image & product title --> */}
-            <Link to={`/product/${product._id}`} className="flex flex-col items-center text-center group">
+            <Link to={`/product/${_id}`} className="flex flex-col items-center text-center group">
                 <div className="w-44 h-48">
-                    <img draggable="false" className="w-full h-full object-contain" src={product.images} alt="" />
+                    <img draggable="false" className="w-full h-full object-contain" src={images} alt="" />
                 </div>
-                <h2 className="text-sm mt-4 group-hover:text-primary-blue text-left">{product.name.length > 85 ? `${product.name.substring(0, 85)}...` : product.name}</h2>
+                <h2 className="text-sm mt-4 group-hover:text-primary-blue text-left">{name.length > 85 ? `${name.substring(0, 85)}...` : name}</h2>
             </Link>
             {/* <!-- image & product title --> */}
 
@@ -42,16 +41,16 @@ const Product = (props) => {
                 {/* <!-- rating badge --> */}
                 <span className="text-sm text-gray-500 font-medium flex gap-2 items-center">
                     <span className="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">
-                        {product.ratings.toFixed(1)} <StarIcon sx={{ fontSize: "14px" }} /></span>
-                    <span>({product.numOfReviews})</span>
+                        {ratings.toFixed(1)} <StarIcon sx={{ fontSize: "14px" }} /></span>
+                    <span>({numOfReviews})</span>
                 </span>
                 {/* <!-- rating badge --> */}
 
                 {/* <!-- price container --> */}
                 <div className="flex items-center gap-1.5 text-md font-medium">
-                    <span>UGX: {product.price.toLocaleString()}</span>
-                    <span className="text-gray-500 line-through text-xs">UGX: {product.cuttedPrice.toLocaleString()}</span>
-                    <span className="text-xs text-primary-green">{getDiscount(product.price, product.cuttedPrice)}%&nbsp;off</span>
+                    <span>UGX: {price.toLocaleString()}</span>
+                    <span className="text-gray-500 line-through text-xs">UGX: {cuttedPrice}</span>
+                    <span className="text-xs text-primary-green">{getDiscount(price, cuttedPrice)}%&nbsp;off</span>
                 </div>
                 {/* <!-- price container --> */}
             </div>
